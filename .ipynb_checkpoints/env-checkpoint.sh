5e1ab39d248e4b9899a59cb3af38be57233917c1
Lchahuas <153758498+Lchahuas@users.noreply.github.com>
2024-06-26 11:27:38 -0500

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/env-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export VERTEX_LOCATION=us-central1
-export VERTEX_PROJECT_ID=durable-ring-405419
-export SOLUTION_NAME=heart-prediction
-export CONTAINER_IMAGE_REGISTRY=${VERTEX_LOCATION}-docker.pkg.dev/${VERTEX_PROJECT_ID}/vertex-images
